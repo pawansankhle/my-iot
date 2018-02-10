445359b8b7bab84645adf3ebff087b9f7265c841
pawansankhle <pawansankhle@gmail.com>
2018-02-10 18:06:39 +0000

    path added

--- dev/common/file_downloader.py
+++ dev/common/file_downloader.py
@@ -1,54 +1,70 @@
 import uos as os
 import usocket as socket
-import config
+import logger
+import urequests
 
+_logger = logger.initialize_logging('micro.log')
+import config
+config.initialize_config()
 _config = config.get_config()
 
 
 # todo: https
 
-def http_get_async(url):
-    _, _, host, path = url.split('/', 3)
-    if ':' in host:
-        host, port = host.split(':')
-    else:
-        port = 80
-    addr = socket.getaddrinfo(host, port)[0][-1]
-    s = socket.socket()
-    s.connect(addr)
-    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host),
-                 'utf8'))
-    started = False
-    header_str = b''
-    headers = {}
-    while True:
-        data = s.recv(1000)
-        if data:
-            body_bytes = data
-            if not started and b'HTTP' in body_bytes:
-                started = True
-                continue
-            buffer = header_str + body_bytes
-            if not headers and not b'\r\n\r\n' in buffer:
-                header_str += body_bytes
-                continue
-            if b'\r\n\r\n' in buffer:
-                headers, body_bytes = buffer.split(b'\r\n\r\n')
-            if body_bytes:
-                yield body_bytes
-        else:
-            break
+# def http_get_async(url):
+    # _, _, host, path = url.split('/', 3)
+    # if ':' in host:
+    #     host, port = host.split(':')
+    # else:
+    #     port = 80
+    # addr = socket.getaddrinfo(host, port)[0][-1]
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.connect(addr)
+    # s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host),
+    #              'utf8'))
+    # started = False
+    # header_str = b''
+    # headers = {}
+    # while True:
+    #     data = s.recv(1000)
+    #     if data:
+    #         body_bytes = data
+    #         if not started and b'HTTP' in body_bytes:
+    #             if b'301' in body_bytes:
+    #                 print(body_bytes)
+    #                 for u in body_bytes.split(b'\r\n'):
+    #                     if b'Location:' in u:
+    #                         url = str(u).split('Location: ')[1].replace("'", "").strip()
+                            
+    #                         # print(url)
+    #                         # start(url)
+                            
+    #             started = True
+    #             continue
+    #         buffer = header_str + body_bytes
+            
+    #         if not headers and not b'\r\n\r\n' in buffer:
+    #             header_str += body_bytes
+    #             continue
+    #         if b'\r\n\r\n' in buffer:
+    #             headers, body_bytes = buffer.split(b'\r\n\r\n')
+            
+            # if body_bytes:
+            #     yield body_bytes
+        # else:
+        #     break
 
 
 def http_get(url):
     response = b''
     try:
-        get = http_get_async(url)
-        while True:
-            file_bytes = get.send(None)
-            response += file_bytes
-    except StopIteration:
-        pass
+        r = urequests.request('GET', url)
+        response += r.content
+        # while True:
+        #     file_bytes = get.send(None)
+        #     response += file_bytes
+    except Exception as ex:
+        print(ex)
 
     response_str = str(response, 'utf-8')
     return response_str
@@ -69,18 +85,19 @@ def http_get_to_file(url, path):
     ensure_dirs(path)
     with open(path, 'w') as outfile:
         try:
-            get = http_get_async(url)
-            while True:
-                file_bytes = get.send(None)
-                outfile.write(file_bytes)
-        except StopIteration:
+            get = http_get(url)
+            # while True:
+            #     file_bytes = get.send(None)
+            outfile.write(get)
+        except Exception as ex:
+            print(ex)
             outfile.close()
 
 def start(url='https://raw.githubusercontent.com/pawansankhle/my-iot/master/dev/'+ _config.get_client_id() +'/update.json'):
     try:
         import ujson as json
-        response = json.loads(http_get(url))
-        print(rresponse)
+        res = http_get(url)
+        response = json.loads(res)
         for file in response['files']:
             path = file['path']
             url = 'https://raw.githubusercontent.com/pawansankhle/my-iot/master/dev/{}'.format(path)
--- reset.sh
+++ reset.sh
@@ -11,7 +11,7 @@ remove_logs(){
 
 init(){
     param=${1}
-    if [ "${param}" == "reset" ]; then
+    if [ "${param}" == "firmware" ]; then
         python esptool.py --port /dev/ttyUSB0 erase_flash
         python esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect -fm dio 0 ${FIRMWARE_FILE_PATH}esp8266-20171101-v1.9.3.bin
     fi
@@ -20,18 +20,16 @@ init(){
         remove_logs;
         OLD_PATH=${pwd};
         cd ${FILES_PATH}
-        echo "coping main.py..."
-        ampy --port /dev/ttyUSB0 put ../esp8266_525a8a00/main.py
-        # echo "coping config.json..."
-        # ampy --port /dev/ttyUSB0 put ${FILES_PATH}config.json
-        echo "coping config.py..."
-        ampy --port /dev/ttyUSB0 put config.py
-        echo "coping wifi.py..."
-        ampy --port /dev/ttyUSB0 put wifi.py
-        echo "coping mqtt_writer.py..."
-        ampy --port /dev/ttyUSB0 put mqtt_writer.py
-        echo "coping logger.py..."
-        ampy --port /dev/ttyUSB0 put logger.py
+        # echo "coping main.py..."
+        # ampy --port /dev/ttyUSB0 put ../esp8266_525a8a00/main.py
+        # echo "coping config.py..."
+        # ampy --port /dev/ttyUSB0 put config.py
+        # echo "coping wifi.py..."
+        # ampy --port /dev/ttyUSB0 put wifi.py
+        # echo "coping mqtt_writer.py..."
+        # ampy --port /dev/ttyUSB0 put mqtt_writer.py
+        # echo "coping logger.py..."
+        # ampy --port /dev/ttyUSB0 put logger.py
         echo "coping file upload.py..."
         ampy --port /dev/ttyUSB0 put file_downloader.py
         cd ${OLD_PATH}
